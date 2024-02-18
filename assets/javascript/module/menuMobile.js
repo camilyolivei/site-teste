c64b04b06fe3d6dbaa3f6f37b2e69453f115235f
cami <camilyguilhermeoliveira@gmail.com>
2024-02-18 17:19:53 -0300

    att
    att

--- assets/javascript/module/menuMobile.js
+++ assets/javascript/module/menuMobile.js
@@ -3,21 +3,28 @@ export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button_open"]');
   const menuList = document.querySelector('[data-menu="list"]');
   const emphasis = document.querySelector(".emphasis");
-  const headerMenu = document.querySelector('header')
+  const headerMenu = document.querySelector("header");
+  const buttonReplacement = document.querySelector(
+    '[data-menu="button_open"] i'
+  );
 
   function openMenu() {
     menuList.classList.add("active");
     menuButton.classList.add("active");
+    buttonReplacement.classList.remove("fa-bars");
+    buttonReplacement.classList.add("fa-xmark");
     outsideClick(menuList, ["click", "touchstart"], () => {
       menuList.classList.remove("active");
       menuButton.classList.remove("active");
+      buttonReplacement.classList.add("fa-bars");
+      buttonReplacement.classList.remove("fa-xmark");
     });
   }
   function effectHeader() {
     const valueSrolly = window.scrollY;
     if (valueSrolly > 10) {
       emphasis.classList.add("active");
-      headerMenu.classList.add('active')
+      headerMenu.classList.add("active");
     } else if (valueSrolly < 10) {
       emphasis.classList.remove("active");
       headerMenu.classList.remove("active");
